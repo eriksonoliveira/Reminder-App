f8636c7ce68f3f48ce1b55e066895e5a1f5e4bfb
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-05-31 02:53:16 -0400

    new updates from delete_reminder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,7 @@
 import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constant';
-<<<<<<< HEAD
-// import { bake_cookie, read_cookie } from 'sfcookies';
-import Cookies from 'universal-cookie';
-
-//Add reminder
-=======
 import { bake_cookie, read_cookie } from 'sfcookies';
 
 //***Reminder reducer***//
->>>>>>> delete_reminder
 const newReminder = (action) => {
   let { text, dueDate, dueTime } = action;
   return {
@@ -19,10 +12,7 @@ const newReminder = (action) => {
   }
 }
 
-<<<<<<< HEAD
 //Remove reminder
-=======
->>>>>>> delete_reminder
 const removeById =(state = [], id) => {
   //Create a new array with all reminders in state except the one that the user
   //wants to remove
@@ -34,43 +24,22 @@ const removeById =(state = [], id) => {
 const reminders = (state = [], action) => {
   let reminders = null;
   //Initialize state with data at reminders cookie if any
-<<<<<<< HEAD
-  const cookies = new Cookies();
-  state = cookies.getAll('reminders');
-  // state = read_cookie('reminders');
-=======
   state = read_cookie('reminders');
->>>>>>> delete_reminder
   switch(action.type) {
     case ADD_REMINDER:
       reminders = [...state, newReminder(action)];
       //Save the content of reminders into a cookie
-<<<<<<< HEAD
-      cookies.set('reminders', reminders, { path: '/', expires: new Date(Date.now()+31592000000) });
-      // bake_cookie('reminders', reminders);
-=======
       bake_cookie('reminders', reminders);
->>>>>>> delete_reminder
       return reminders;
 
       case DELETE_REMINDER:
         reminders = removeById(state, action.id);
-<<<<<<< HEAD
-        cookies.set('reminders', reminders, { path: '/', expires: new Date(Date.now()+31592000000) });
-        // bake_cookie('reminders', reminders);
-=======
         bake_cookie('reminders', reminders);
->>>>>>> delete_reminder
         return reminders;
 
       case CLEAR_REMINDERS:
         reminders = [];
-<<<<<<< HEAD
-        cookies.set('reminders', reminders, { path: '/', expires: new Date(Date.now()+31592000000) });
-        // bake_cookie('reminders', reminders);
-=======
         bake_cookie('reminders', reminders);
->>>>>>> delete_reminder
         return reminders;
     default:
       return state;
